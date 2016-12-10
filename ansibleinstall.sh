2dac8b4ab8423630c112f2899279bf3df336f7dc
psachdev6375 <puneet.sachdev@gmail.com>
2016-12-10 20:57:17 +0530

    Added Jenkins install in ansiblecontroller provisioning script

--- ansibleinstall.sh
+++ ansibleinstall.sh
@@ -8,3 +8,12 @@ sudo apt-get install -y ansible
 
 #Install Python PiP Manager
 sudo apt-get install -y python-pip
+
+#install JDK 
+sudo apt-get install -y default-jre 
+
+#Install Jenkins 
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update 
+sudo apt-get install -y jenkins 
